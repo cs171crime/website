30cf12670d1c6469e83ed146716fb1a48fc1adfd
Kimia Mavon <kimia@Kimias-MacBook-Air.local>
2018-11-26 16:17:15 -0500

    update

--- js/crime.js
+++ js/crime.js
@@ -1,263 +1,263 @@
 
-    var margin = {
+var margin = {
 
-        top: 40,
-        right: 40,
-        bottom: 40,
-        left: 60
-    };
-    var colorscale = d3.scale.quantize();
-    var width = 700;
-    var height = 580;
-    var inputValue = null;
-    var total = {};
-    var svg = d3.select( "#chart-area")
-        .append( "svg" )
-        .attr( "width", width )
-        .attr( "height", height );
+    top: 40,
+    right: 40,
+    bottom: 40,
+    left: 60
+};
+var colorscale = d3.scale.quantize();
+var width = 700;
+var height = 580;
+var inputValue = null;
+var total = {};
+var svg = d3.select( "#chart-area")
+    .append( "svg" )
+    .attr( "width", width )
+    .attr( "height", height );
 
-    var g = svg.append( "g" );
+var g = svg.append( "g" );
 
-    var albersProjection = d3.geoAlbers()
-        .scale( 190000 )
-        .rotate( [71.057,0] )
-        .center( [0, 42.313] )
-        .translate( [width/2,height/2] );
+var albersProjection = d3.geoAlbers()
+    .scale( 190000 )
+    .rotate( [71.057,0] )
+    .center( [0, 42.313] )
+    .translate( [width/2,height/2] );
 
-    var geoPath = d3.geoPath()
-        .projection( albersProjection );
-    var legendOptions = {
-        leftOffset: 120
-    };
+var geoPath = d3.geoPath()
+    .projection( albersProjection );
+var legendOptions = {
+    leftOffset: 120
+};
 
-    // set up d3 tooltip
-    var tip = d3.tip()
-        .attr('class', 'd3-tip');
-    svg.call(tip); //maybe this should be SVG/g???
+// set up d3 tooltip
+var tip = d3.tip()
+    .attr('class', 'd3-tip');
+svg.call(tip); //maybe this should be SVG/g???
 
-    var colorlist = {};
+var colorlist = {};
 
-    var country_data, ID_data = {};
+var country_data, ID_data = {};
 
-    var rawcountry_data, ID_data = {};
-
-
-    // Use the Queue.js library to read two files
-    queue()
-        .defer(d3.json, "data/boston.geojson")
-        .defer(d3.csv, "data/hoods.csv")
-        .await(function(error, mapTopJson, rawcountry_data) {
+var rawcountry_data, ID_data = {};
 
 
-            rawcountry_data.forEach(function(d) {
-                d.Heroin= d.Heroin;
-                d.Marijuana=d.Marijuana;
-                d.Heroin = parseFloat(d.Heroin);
-                d.Marijuana = parseFloat(d.Marijuana);
-                d.index = parseFloat(d.index);
-            });
-
-            country_data = rawcountry_data.filter(function (d) {
-                return d.index < 100;});
+// Use the Queue.js library to read two files
+queue()
+    .defer(d3.json, "data/boston.geojson")
+    .defer(d3.csv, "data/hoods.csv")
+    .await(function(error, mapTopJson, rawcountry_data) {
 
 
-            country_data.forEach(function(d) {
-                ID_data[d.neighborhood] = d;
-
-            });
-            var geoJSON_africa = mapTopJson.features;
-
-            g.selectAll( "path" )
-                .data( geoJSON_africa )
-                .enter()
-                .append( "path" )
-                .attr("class", "map")
-                .attr( "d", geoPath );
-
-            updateChoropleth();
-
-
-
-        });
-    function updateChoropleth() {
-
-
-        var data_ = d3.select("#selector")
-            .node()
-            .value;
-
-        var data_Values = country_data.map(function(d) {
-            return d[data_];
+        rawcountry_data.forEach(function(d) {
+            d.Heroin= d.Heroin;
+            d.Marijuana=d.Marijuana;
+            d.Heroin = parseFloat(d.Heroin);
+            d.Marijuana = parseFloat(d.Marijuana);
+            d.index = parseFloat(d.index);
         });
 
-
-         var colorlist =  color_by_type(data_);
-
-
-        colorscale
-            .domain(d3.extent(data_Values))
-            .range(colorlist);
-        
+        country_data = rawcountry_data.filter(function (d) {
+            return d.index < 100;});
 
 
-        svg.selectAll(".map")
-            .attr("fill", function(d) {
+        country_data.forEach(function(d) {
+            ID_data[d.neighborhood] = d;
 
-                return retrievevalue(d, data_); })
-            .on("mouseover", function(d){
-                d3.select("h4").text(d.properties.name).style("stroke", "white");
+        });
+        var geoJSON_africa = mapTopJson.features;
 
-            })
-            .on("mouseout", function(d){
-                d3.select("h4").text("").style("stroke", "white");
+        g.selectAll( "path" )
+            .data( geoJSON_africa )
+            .enter()
+            .append( "path" )
+            .attr("class", "map")
+            .attr( "d", geoPath );
 
-            });
+        updateChoropleth();
 
 
 
-
-        var legend = svg.selectAll('g.legry')
-            .data(colorscale.range(), function(d) {
-
-                return d;
-            });
+    });
+function updateChoropleth() {
 
 
-        var legend_ = legend.enter()
-            .append('g')
-            .attr('class', 'legry');
+    var data_ = d3.select("#selector")
+        .node()
+        .value;
+
+    var data_Values = country_data.map(function(d) {
+        return d[data_];
+    });
+
+
+    var colorlist =  color_by_type(data_);
+
+
+    colorscale
+        .domain(d3.extent(data_Values))
+        .range(colorlist);
 
 
 
-        legend_
-            .append('rect')
-            .attr("x", width - 270)
-            .attr("y", function(d, i) {
-                return i * 20 +320;
-            })
-            .attr("width", 20)
-            .attr("height", 20)
-            .style("stroke", "black")
-            .style("stroke-width", 1)
-            .style("fill", function(d){
-                return d;
-            });
+    svg.selectAll(".map")
+        .attr("fill", function(d) {
 
+            return retrievevalue(d, data_); })
+        .on("mouseover", function(d){
+            d3.select("h4").text(d.properties.name).style("stroke", "white");
 
-        legend_
-            .append('text')
-            .attr("x", width-245 )
-            .attr("y", function(d, i) {
-                return i * 20 +335;
-            })
-            .style("stroke", "white")
-            .style("stroke-width", 1);
+        })
+        .on("mouseout", function(d){
+            d3.select("h4").text("").style("stroke", "white");
 
-        legend.selectAll('rect')
-            .style("fill", function(d) {
-
-                return d;
-            });
-
-        legend_.selectAll('text')
-            .text(function(d, i) {
-                var extent = colorscale.invertExtent(d);
-                format = d3.format(",.2r");
-
-                boundary = format(+extent[0]);
-                boundary2 = format(+extent[1]);
-                console.log("BOUNDARY", boundary)
-                var total = boundary+ " - " + boundary2 + grouptext(data_);
-
-
-                return total;
-            });
-        console.log("LEGEND2", legend)
-
-        legend.exit().remove();
+        });
 
 
 
 
+    var legend = svg.selectAll('g.legry')
+        .data(colorscale.range(), function(d) {
 
-    }
-
-    function grouptext(data_) {
-        if (data_ === "Heroin" ) {
-            return " Heroin reports"
-        } else {
-            return " Marijuana reports";
-        };
-    }
+            return d;
+        });
 
 
-    function calculateFill(d, data_) {
-        var countryData = retrievevalue(d);
+    var legend_ = legend.enter()
+        .append('g')
+        .attr('class', 'legry');
 
-        if (countryData !== null) {
-            var metricValue = countryData[data_];
-            if (isNaN(metricValue)) {
 
-                return "black";
-            } else {
 
-                return colorscale(metricValue);
-            }
-        } else {
+    legend_
+        .append('rect')
+        .attr("x", width - 270)
+        .attr("y", function(d, i) {
+            return i * 20 +320;
+        })
+        .attr("width", 20)
+        .attr("height", 20)
+        .style("stroke", "black")
+        .style("stroke-width", 1)
+        .style("fill", function(d){
+            return d;
+        });
+
+
+    legend_
+        .append('text')
+        .attr("x", width-245 )
+        .attr("y", function(d, i) {
+            return i * 20 +335;
+        })
+        .style("stroke", "white")
+        .style("stroke-width", 1);
+
+    legend.selectAll('rect')
+        .style("fill", function(d) {
+
+            return d;
+        });
+
+    legend_.selectAll('text')
+        .text(function(d, i) {
+            var extent = colorscale.invertExtent(d);
+            format = d3.format(",.2r");
+
+            boundary = format(+extent[0]);
+            boundary2 = format(+extent[1]);
+            console.log("BOUNDARY", boundary)
+            var total = boundary+ " - " + boundary2 + grouptext(data_);
+
+
+            return total;
+        });
+    console.log("LEGEND2", legend)
+
+    legend.exit().remove();
+
+
+
+
+
+}
+
+function grouptext(data_) {
+    if (data_ === "Heroin" ) {
+        return " Heroin reports"
+    } else {
+        return " Marijuana reports";
+    };
+}
+
+
+function calculateFill(d, data_) {
+    var countryData = retrievevalue(d);
+
+    if (countryData !== null) {
+        var metricValue = countryData[data_];
+        if (isNaN(metricValue)) {
 
             return "black";
+        } else {
+
+            return colorscale(metricValue);
         }
+    } else {
+
+        return "black";
     }
+}
 
 
-    function retrievevalue(d, data_) {
-        console.log('retrieve');
-        var country_data2 = getit(d);
-        var data_Value = country_data2[data_];
-        console.log("colorscale(data_Value))", colorscale(data_Value));
-        return colorscale(data_Value);
+function retrievevalue(d, data_) {
+    console.log('retrieve');
+    var country_data2 = getit(d);
+    var data_Value = country_data2[data_];
+
+    return colorscale(data_Value);
+}
+
+function getit(d) {
+    console.log('ID_data[d.properties.name]', ID_data[d.properties.name]);
+    return ID_data[d.properties.name] || 0;
+
+}
+
+
+function color_by_type(data_) {
+
+    if (data_ == "Heroin" ) {
+        return [
+
+
+            '#fcbba1',
+            '#fb6a4a',
+            '#cb181d',
+            '#67000d'
+
+        ];
     }
+    else if (data_ == "Marijuana" ) {
+        return  [
 
-    function getit(d) {
-        console.log('ID_data[d.properties.name]', ID_data[d.properties.name]);
-        return ID_data[d.properties.name] || 0;
 
+            '#d9f0a3',
+            '#78c679',
+            '#238443',
+            '#004529'
+
+        ];
     }
+    else {
+        return [
 
+            '#ffffff',
+            '#ffffff',
+            '#ffffff',
+            '#ffffff'
 
-    function color_by_type(data_) {
-
-        if (data_ == "Heroin" ) {
-            return [
-
-
-                '#fcbba1',
-                '#fb6a4a',
-                '#cb181d',
-                '#67000d'
-
-            ];
-        }
-        else if (data_ == "Marijuana" ) {
-            return  [
-
-
-                '#d9f0a3',
-                '#78c679',
-                '#238443',
-                '#004529'
-
-            ];
-        }
-        else {
-            return [
-
-                '#ffffff',
-                '#ffffff',
-                '#ffffff',
-                '#ffffff'
-
-            ];
-        };
-    }
+        ];
+    };
+}
 
